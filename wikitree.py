7a4c76e73457cccd00a008dfc1bb428f1f824a81
frederikgram <frederikxyz@hotmail.com>
2018-11-30 10:07:44 +0100

    adjustments

--- wikitree.py
+++ wikitree.py
@@ -14,38 +14,41 @@ class Node:
         self.children = []
 
 
-def get_sublinks(url: str) -> list:
+def get_links(_url: str) -> list:
     """
-    :param url: str(url) with base_path https://{lang}.wikipedia.org/wiki/
+    :param _url: str(url) with base_path https://{lang}.wikipedia.org/wiki/
     :return: list of str(links)
     """
-    r = session.get(url)
+    r = session.get(_url)
 
-    return [link for link in r.html.absolute_links if  # Basic filtering included
-            BASE_PATH in link and ":" not in link[len(BASE_PATH):] and link != url]
+    return [link for link in r.html.absolute_links if  # Basic filtering
+            BASE_PATH in link and ":" not in link[len(BASE_PATH):] and link != _url]
 
 
-def generate_tree(root: Node, max_depth: int=0) -> None:
+def generate_tree(_root: Node, max_depth: int=0) -> None:
     """ # Breadth First Search (generation)
-    :param root: node of type Node
+    :param _root: node of type Node
     :param max_depth: Optional, set a max article depth as an integer
-    :return: Generates a tree structures with max_depth from root
+    :return: Generates a tree structures with max_depth from root, if max_depth is None, return list of generated nodes.
     """
 
     nodes = []
-    stack = [root]
+    stack = [_root]
     depth = 0
 
     links_per_article = 5
 
-    max_depth = max_depth or links_per_article ** 3
+    # Set default value for tree depth
+    if max_depth == 0:
+        max_depth = links_per_article ** 3
 
-    while stack and depth < max_depth:
+    # Generate nodes using Breadth First Search
+    while stack and max_depth is None or depth < max_depth:
         c_node = stack[0]
         stack = stack[1:]
 
         nodes.append(c_node)
-        for link in get_sublinks(c_node.link)[:links_per_article]:
+        for link in get_links(c_node.link)[:links_per_article]:
             n = Node(link=link, parent=c_node)
             stack.append(n)
             c_node.children.append(n)
@@ -53,16 +56,16 @@ def generate_tree(root: Node, max_depth: int=0) -> None:
         depth += 1
 
 
-def visualize(node: Node, indent: int=0) -> None:
+def visualize(_node: Node, indent: int=0) -> None:
     """ # Recursive Depth First Search
-    :param node: node of type Node
+    :param _node: node of type Node
     :param indent: int(x) how many dashes should be added per depth level
     :return: Visualizes a tree structures to stdout using sys.stdout.write
     """
-    sys.stdout.write("[{0}] {1}: {2}\n".format(str(indent // 2), '-'*indent, node.link))
+    sys.stdout.write("[{0}] {1}: {2}\n".format(str(indent // 2), '-' * indent, _node.link))
 
     indent += 2
-    for child in node.children:
+    for child in _node.children:
         visualize(child, indent=indent)
 
 
@@ -71,7 +74,7 @@ if __name__ == "__main__":
     lang = "en"
     BASE_PATH = "https://{lang}.wikipedia.org/wiki/".format(lang=lang)
 
-    query = input("""topic (e.g. "physics"): """) or "physics"
+    query = input("Topic: ") or "physics"
     query.replace(' ', '_')
 
     url = os.path.join(BASE_PATH, query)
